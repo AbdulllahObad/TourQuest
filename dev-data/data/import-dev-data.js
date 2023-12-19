edcb306e3062818f49c70af8d70710d962bdcf54
AbdulllahObad <95959487+AbdulllahObad@users.noreply.github.com>
2023-12-18 23:40:59 -0700

    Importing Development Data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -23,6 +23,17 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
 );
 
+// IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
 // DELETE ALL DATA FROM DB
 const deleteData = async () => {
   try {
@@ -33,3 +44,11 @@ const deleteData = async () => {
   }
   process.exit();
 };
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+console.log(process.argv);
